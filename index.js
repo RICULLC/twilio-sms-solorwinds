eb9edb774892fc3de1c7ef9ad9d2ab90de6a1e3e
Akshay Simha <akshay.simha91@gmail.com>
2021-08-03 21:05:59 +0530

    send_sms changed to post request

--- index.js
+++ index.js
@@ -3,31 +3,41 @@ const twilio = require('twilio');
 
 const app = express()
 const port = process.env.PORT
-
+var router = express.Router();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
 const authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
 
 const client = new twilio(accountSid, authToken);
 
-
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/send_sms', (req, res) => {
-    client.messages
-    .create({
-        body: 'Hello from Node',
-        to: '+918892538433', // Text this number
-        from: process.env.TWILIO_NUMBER, // From a valid Twilio number
+router.post('/send_sms', (req, res) => {
+    const { message, recipients } = req.body
+    Promise.all(
+        recipients.map(recipient => {
+            client.messages
+            .create({
+                body: message,
+                to: recipient, // Text this number
+                from: process.env.TWILIO_NUMBER, // From a valid Twilio number
+            })
+        }))
+    .then((message) => {
+        console.log('message sent')
+        res.send(message.sid)
     })
-    .then((message) => res.send(message.sid))
     .catch(err => {
         console.log(err)
         res.send(err.message)
     })  
 })
+
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
