ea380a67473a28088491ee3515dc825a5421eeb4
Akshay Simha <akshay.simha91@gmail.com>
2021-08-03 16:50:56 +0530

    updated port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const twilio = require('twilio');
 
 const app = express()
-const port = 3000
+const port = process.env.PORT ? 3000 : 80
 
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
